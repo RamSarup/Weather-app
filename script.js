32824cb7f9aaa5a79669bd273fd49f563e48607d
RamSarup <ramsarup516@gmail.com>
2025-12-15 19:00:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const url = "https://api.weatherbit.io/v2.0/current?lat=35.7721&lon=-78.63861&key=XXX&units=I";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let temperature = 20;
+// document.querySelector(".temp-main").textContent = `${temperature}°C`;
+
+// let humidity = 50;
+// document.querySelector("#h").textContent = `${humidity}%`;
+
+// let windSpeed = 90;
+// document.querySelector("#w").textContent = `${windSpeed} km/h`;
